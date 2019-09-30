0e47cf367aedefdba91ca86818f0979333fa521a
xiaoqiang-zhao <longze_xq@163.com>
2019-09-30 13:30:04 +0800

    第二个 activity 启动

---
+++ app/src/main/java/com/xiaoqiangzhao/android_hello_world/DisplayMessageActivity.java
@@ -0,0 +1,14 @@
+package com.xiaoqiangzhao.android_hello_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
--- app/src/main/java/com/xiaoqiangzhao/android_hello_world/MainActivity.java
+++ app/src/main/java/com/xiaoqiangzhao/android_hello_world/MainActivity.java
@@ -7,10 +7,15 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,4 +51,22 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    /** Called when the user clicks the Send button */
+    public void search(View view) {
+        // Do something in response to button
+
+        EditText editText = (EditText) findViewById(R.id.searchInputEditText);
+        String message = editText.getText().toString();
+
+        // 写入到第一个文本组件中
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        textView1.setText(message);
+
+         Intent intent = new Intent(this, DisplayMessageActivity.class);
+         intent.putExtra(EXTRA_MESSAGE, message);
+         startActivity(intent);
+    }
+
 }
